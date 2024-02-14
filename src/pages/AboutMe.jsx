41440d57dcc04086dca931e866dd5676c5401bda
Bixey <mida.santiago98@gmail.com>
2024-02-14 06:50:22 -0500

    Updated About Me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <div>
-            <h2>About Me</h2>
-            <p>
+        <div className='max-w-xl mx-auto mt-8 p-4 text-white bg-gray-800 rounded-md shadow-md'>
+            <h2 className='text-3xl font-bold mb-4'>About Me</h2>
+            <img src='https://placekitten.com/200/200' className='mx-auto'></img>
+            <p className='text-base'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                 ut labore et dolore magna aliqua. Sed lectus vestibulum mattis ullamcorper velit.
                 Tellus cras adipiscing enim eu turpis egestas pretium aenean. Enim neque volutpat ac
