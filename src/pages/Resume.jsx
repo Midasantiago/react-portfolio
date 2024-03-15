751256168aa9c1adf2704909696cac6c0ca628ae
Bixey <mida.santiago98@gmail.com>
2024-03-15 00:00:39 -0400

    Updated Resume Download Path/Directory

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     return (
         <div className='max-w-xl mx-auto mt-8 p-4 bg-gray-800 text-white'>
             <h2 className='text-2xl font-bold mb-4'> 
-                <a href='../../assets/resume/SantiagoResume.docx' download="SantiagoResume.docx" className='block bg-blue-500 text-white py-2 px-4 rounded text-center hover:bg-blue-600 transition duration-300'>
+                <a href='=/resume/SantiagoResume.docx' download="SantiagoResume.docx" className='block bg-blue-500 text-white py-2 px-4 rounded text-center hover:bg-blue-600 transition duration-300'>
                     Download Resume
                 </a>
             </h2>
